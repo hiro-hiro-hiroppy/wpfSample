a6c976a67227c64950e3c9e4d67c39bc24a7421e
ひろゆき <hiroyuki-yokota-20211001@outlook.jp>
2022-02-20 15:42:37 +0900

    WPFの勉強終わり

---
+++ wpfSample/026_ToolBar.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfSample
+{
+    /// <summary>
+    /// _026_ToolBar.xaml の相互作用ロジック
+    /// </summary>
+    public partial class _026_ToolBar : Window
+    {
+        public _026_ToolBar()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
